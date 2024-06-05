798cebae25ab6907b51307e43f5be5a0aaaf808a
Maksim Losjuk <m.lysuk@gmail.com>
2024-06-05 13:58:31 +0200

    main page skeleton created

--- client/src/components/Homepage/Map.jsx
+++ client/src/components/Homepage/Map.jsx
@@ -1,5 +1,16 @@
+import "./Map.css";
+
 function Map() {
-  return <h2>MAP</h2>;
+  return (
+    <div className="map-component">
+      <div className="input container">
+        <p>Input</p>
+      </div>
+      <div className="map container">
+        <h2>MAP</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Map;
--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -1,5 +1,18 @@
+import { MdElectricCar } from "react-icons/md";
+import "./NavBar.css";
+
 function NavBar() {
-  return <h4>NAVBAR</h4>;
+  return (
+    <div className="navBar_component">
+      <div className="menuBurger">
+        <span />
+        <span />
+      </div>
+      <div className="logo">
+        <MdElectricCar />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;

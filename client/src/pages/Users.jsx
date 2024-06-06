75cb820314db674b4ff60c7bdad73094561ff897
flakronbajrami <103247082+koni0669@users.noreply.github.com>
2024-06-06 10:43:49 +0200

    Delete Users.jsx

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,12 +1,1 @@
-import { useLoaderData } from "react-router-dom";
 
-function Users() {
-  const cars = useLoaderData();
-  return (
-    <div>
-      <p>{cars}</p>
-    </div>
-  );
-}
-
-export default Users;

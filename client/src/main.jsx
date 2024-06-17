35781c38127a700b18c09a0a9c48abb43062595e
rostom44 <blackgamerdv7@gmail.com>
2024-06-17 20:29:39 +0200

    git has a problem for detecting changed name of files so i'm fixing it

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,12 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pagescopy/HomePage";
 
 // page components
-import AboutUsPage from "./pages/AboutUsPage";
-import ContactPage from "./pages/ContactPage";
-import ProfilePage from "./pages/ProfilePage";
+import AboutUsPage from "./pagescopy/AboutUsPage";
+import ContactPage from "./pagescopy/ContactPage";
+import ProfilePage from "./pagescopy/ProfilePage";
 
 const stationApi = import.meta.env.VITE_API_URL;
 
@@ -42,8 +42,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
-// test
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

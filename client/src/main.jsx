8d7b0e425651fe0e90c0115002cd20c0acdee37b
rostom44 <blackgamerdv7@gmail.com>
2024-06-26 23:31:00 +0200

    moved the cars and user api to another file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
@@ -16,31 +17,11 @@ import ContactPage from "./pages/ContactPage";
 import ProfilePage from "./pages/ProfilePage";
 import ProfileAccess from "./pages/ProfileAccess";
 import SignUp from "./pages/SignUp";
+import CarSignUp from "./pages/SignUpCar";
 import SignIn from "./components/SignIn/SignIn";
 import EditProfile from "./pages/EditProfile";
 
 const Api = import.meta.env.VITE_API_URL;
-
-const handleSignUp = async ({ formData }) => {
-  try {
-    const response = await fetch(`${Api}/api/users`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-
-    if (response.status !== 201) {
-      const errorData = await response.json();
-      return { error: errorData.message };
-    }
-
-    return { success: true };
-  } catch (error) {
-    return { error: error.message };
-  }
-};
 
 const router = createBrowserRouter([
   {
@@ -58,7 +39,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/signup",
-        element: <SignUp handleSignUp={handleSignUp} />,
+        element: <SignUp />,
+      },
+      {
+        path: "/signup/car",
+        element: <CarSignUp />,
       },
       {
         path: "/signIn",

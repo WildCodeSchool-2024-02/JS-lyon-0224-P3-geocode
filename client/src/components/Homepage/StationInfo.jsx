a60b9731afdfc426c4d93280d5776e61a4f690da
rostom44 <blackgamerdv7@gmail.com>
2024-07-06 17:11:14 +0200

    temp

--- client/src/components/Homepage/StationInfo.jsx
+++ client/src/components/Homepage/StationInfo.jsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
-import "./StationInfo.css";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import "./StationInfo.css";
 import stationPic from "../../assets/image/pngtree-white-electric-vehicle-charging-station-png-image_6574430 1.png";
 import chargerPic from "../../assets/image/ev-plug-t2.svg";
-import ReservationPopUp from "./ReservationPopUp";
 
 function StationInfo({ station }) {
-  const address = station !== null ? station.address : "";
-  const power = station !== null ? station.power : "";
-  const spot = station !== null ? station.spots : "";
-  const type = station !== null ? station.type : "";
+  const address = station?.address || "";
+  const power = station?.power || "";
+  const spot = station?.spots || "";
+  const type = station?.type || "";
+  const navigate = useNavigate();
+  const isSelected = station !== null;
 
-  // State to manage the popup visibility
-  const [showPopup, setShowPopup] = useState(false);
-
-  // Handler for the reservation button
   const handleReservation = () => {
-    setShowPopup(true);
+    if (isSelected) {
+      navigate(`/rent/${station.id}`);
+    }
   };
 
   return (
@@ -54,43 +53,36 @@ function StationInfo({ station }) {
             </h3>
           </div>
           <div className="supplementary_buttons">
-            <button type="button" className="button dire">
+            <button
+              type="button"
+              className="button dire"
+              disabled={!isSelected}
+            >
               <h3>Direction</h3>
             </button>
             <button
               type="button"
               className="button"
               onClick={handleReservation}
+              disabled={!isSelected}
             >
               <h3>Reservation</h3>
             </button>
           </div>
         </div>
       </div>
-      {showPopup && (
-        <ReservationPopUp
-          station={station}
-          onClose={() => setShowPopup(false)}
-          onReserved={() => {
-            setShowPopup(false);
-          }}
-        />
-      )}
     </div>
   );
 }
 
 StationInfo.propTypes = {
   station: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     address: PropTypes.string,
     power: PropTypes.number,
     spots: PropTypes.number,
     type: PropTypes.string,
-  }),
-};
-
-StationInfo.defaultProps = {
-  station: null,
+  }).isRequired,
 };
 
 export default StationInfo;

7c98078217aaf9be1fab456ea646ec7d64efc8f9
rostom44 <blackgamerdv7@gmail.com>
2024-07-10 23:23:31 +0200

    another changes

--- client/src/components/reservation/ReservationForm.jsx
+++ client/src/components/reservation/ReservationForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
-import handleRent from "../../utils/HandleRent";
+import handleRent from "../../API/HandleRent";
 
 function ReservationForm({ stationId, userId }) {
   const navigate = useNavigate();
